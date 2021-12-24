2b36a75453bdd1a0d89fa9dff874e7126c8b41c3
Ardhiyan Abdul Hakim <ardhiyan.hakim@mhs.unsoed.ac.id>
2021-12-24 15:04:37 +0700

    Menambahkan script tampilan routing per halaman sesuai layout

---
+++ controllers/ContentController.js
@@ -0,0 +1,56 @@
+module.exports = {
+  html1: (req, res) => {
+    res.render("pages/content/learn-html/html1", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar HTML",
+    });
+  },
+  html2: (req, res) => {
+    res.render("pages/content/learn-html/html2", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar HTML",
+    });
+  },
+  html3: (req, res) => {
+    res.render("pages/content/learn-html/html3", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar HTML",
+    });
+  },
+  css1: (req, res) => {
+    res.render("pages/content/learn-css/css1", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar CSS",
+    });
+  },
+  css2: (req, res) => {
+    res.render("pages/content/learn-css/css2", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar CSS",
+    });
+  },
+  css3: (req, res) => {
+    res.render("pages/content/learn-css/css3", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar CSS",
+    });
+  },
+  js1: (req, res) => {
+    res.render("pages/content/learn-js/js1", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar Javascript",
+    });
+  },
+  js2: (req, res) => {
+    res.render("pages/content/learn-js/js2", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar Javascript",
+    });
+  },
+  js3: (req, res) => {
+    res.render("pages/content/learn-js/js3", {
+      layout: "layouts/content-layout",
+      title: "Mulai Belajar Javascript",
+    });
+  },
+};
